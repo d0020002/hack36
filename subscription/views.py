35d8ddd07dd5c75757aa56773a816e836744bc51
RakeshRoxx <rakeshmandal651@gmail.com>
2021-04-10 19:53:36 +0530

    twilio added

--- subscription/views.py
+++ subscription/views.py
@@ -2,6 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from subscription import models as sub_models
+from twilio.rest import Client
+# fill twilio acc details here
+client = Client(ACCOUNT_SID, AITH_ID)
+twilio_no = 'twilio_number'
 
 
 @api_view(['POST'])
@@ -14,13 +18,19 @@ def register_student(request):
     if data["type"] == "registration":
         data["class"] = request.data.get('class')
         if data["class"] is None:
-            # TODO send sms here
-            return Response("Class not provided",status=status.HTTP_400_BAD_REQUEST)
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Class not provided', to=data["phone"])
+            return Response("Class not provided", status=status.HTTP_400_BAD_REQUEST)
         if data["name"] is None:
-            # TODO send sms here
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Name not provided', to=data["phone"])
             return Response("Name not provided", status=status.HTTP_400_BAD_REQUEST)
         if data["phone"] is None:
-            # TODO send sms here
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Phone not provided', to=data["phone"])
             return Response("Phone not provided", status=status.HTTP_400_BAD_REQUEST)
         student = sub_models.Student.objects.get(
             student_name__exact=data["name"],
@@ -28,7 +38,9 @@ def register_student(request):
             student_class__exact=data["class"]
         )
         if student is not None:
-            # TODO send sms here duplicate registration
+           # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Student exist', to=data["phone"])
             return Response("student exists", status=status.HTTP_409_CONFLICT)
         student = sub_models.Student()
         student.student_class = data["class"]
@@ -36,4 +48,6 @@ def register_student(request):
         student.student_phone = data["phone"]
         student.save()
     # TODO send sms here
+        message = client.messages.create(
+            from_='twilio_no', body='Registered', to=data["phone"])
     return Response("successful", status=status.HTTP_201_CREATED)
