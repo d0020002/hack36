859211a093f1a537fca07c27c95a58654ac3bd4c
d0020002 <d0020002@gmail.com>
2021-04-10 22:35:06 +0530

    Update urls.py

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,4 +3,5 @@ from subscription import views as sub_views
 
 urlpatterns = [
     path('register/', sub_views.register_student, name='register'),
+    path('search/', sub_views.search, name='search'),
 ]
