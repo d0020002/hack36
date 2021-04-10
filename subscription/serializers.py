851e3e6c7139d12d7b8c1a94911c2fa69dae9260
Chandan <chandanmahto00761@gmail.com>
2021-04-10 21:12:19 +0530

    added registration, subjects, topics and progress

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework.serializers import ModelSerializer
+from subscription.models import Topic
+
+
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['__all__']
--- subscription/views.py
+++ subscription/views.py
@@ -1,7 +1,18 @@
+from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from subscription import models as sub_models
+from subscription.serializers import TopicSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from subscription import models as sub_models
+from twilio.rest import Client
+# fill twilio acc details here
+client = Client(settings.ACCOUNT_SID, settings.AUTH_ID)
+twilio_no = settings.TWILIO_NO
 
 
 @api_view(['POST'])
@@ -14,13 +25,19 @@ def register_student(request):
     if data["type"] == "registration":
         data["class"] = request.data.get('class')
         if data["class"] is None:
-            # TODO send sms here
-            return Response("Class not provided",status=status.HTTP_400_BAD_REQUEST)
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Class not provided', to=data["phone"])
+            return Response("Class not provided", status=status.HTTP_400_BAD_REQUEST)
         if data["name"] is None:
-            # TODO send sms here
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Name not provided', to=data["phone"])
             return Response("Name not provided", status=status.HTTP_400_BAD_REQUEST)
         if data["phone"] is None:
-            # TODO send sms here
+            # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Phone not provided', to=data["phone"])
             return Response("Phone not provided", status=status.HTTP_400_BAD_REQUEST)
         student = sub_models.Student.objects.get(
             student_name__exact=data["name"],
@@ -28,7 +45,9 @@ def register_student(request):
             student_class__exact=data["class"]
         )
         if student is not None:
-            # TODO send sms here duplicate registration
+           # twilio added
+            message = client.messages.create(
+                from_='twilio_no', body='Student exist', to=data["phone"])
             return Response("student exists", status=status.HTTP_409_CONFLICT)
         student = sub_models.Student()
         student.student_class = data["class"]
@@ -36,4 +55,25 @@ def register_student(request):
         student.student_phone = data["phone"]
         student.save()
     # TODO send sms here
+        message = client.messages.create(
+            from_='twilio_no', body='Registered', to=data["phone"])
     return Response("successful", status=status.HTTP_201_CREATED)
+
+
+def next_topic(phone):
+    students = sub_models.Student.objects.filter(student_phone__exact=phone)
+    for student in students:
+        progress = sub_models.Progress.objects.get(student=student)
+        if len(progress) != 0:
+            if progress.last_topic_index == sub_models.Topic.objects.filter(chapter=progress.last_chapter).count():
+                progress.last_topic_index = 1
+                progress.last_chapter_index += 1
+                progress.last_chapter = sub_models.Chapter.objects.get(index=progress.last_chapter_index)
+                progress.last_topic = sub_models.Topic.objects.get(chapter=progress.last_chapter,
+                                                                   index=progress.last_topic_index)
+                return TopicSerializer(progress.last_topic)
+            else:
+                progress.last_topic_index += 1
+                progress.last_topic = sub_models.Topic.objects.get(chapter=progress.last_chapter,
+                                                                   index=progress.last_topic_id)
+                return TopicSerializer(progress.last_topic)
